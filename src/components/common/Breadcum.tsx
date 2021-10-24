3ffdb730991535a6ac0d54619c91597232a49b40
luankat0306 <votriluan99@gmail.com>
2021-10-25 00:55:40 +0700

    #luanvt #fix breadcrumbs

--- src/components/common/Breadcum.tsx
+++ src/components/common/Breadcum.tsx
@@ -13,16 +13,17 @@ const BreadcrumbsCustom: FC<BreadcrumbsCustomProps> = ({ breadcrumbs }) => {
   return (
     <Breadcrumbs aria-label="breadcrumb">
       {breadcrumbs.map((item, idx) => {
-        return
-        {
-          !item.isActive ? (
-            <Link key={idx} underline="hover" color="inherit" href={item.link}>
-              {item.label}
-            </Link>
-          ) : (
+        if (item.isActive) {
+          return (
             <Typography key={idx} sx={{ fontWeight: 'bold' }} color="text.primary">
               {item.label}
             </Typography>
+          )
+        } else {
+          return (
+            <Link key={idx} underline="hover" color="inherit" href={item.link}>
+              {item.label}
+            </Link>
           )
         }
       })}
